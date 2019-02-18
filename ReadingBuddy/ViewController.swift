30b22353e7dec6e3d9bf2112024220086d953f6a
Hoang Luong <hoangluong@hoangs-macbook-air-2.local>
2019-02-19 07:51:35 +1000

    Initial Commit

---
+++ ReadingBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingBuddy
+//
+//  Created by Hoang Luong on 19/2/19.
+//  Copyright © 2019 Hoang Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
